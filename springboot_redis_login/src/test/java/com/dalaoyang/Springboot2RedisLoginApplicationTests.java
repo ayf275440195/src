317191a6f3b347d009cbffcf0a4507b70af853c1
aoyufeng <aoyufeng@cptec.cn>
2019-11-21 16:38:46 +0800

    redis登录

---
+++ springboot_redis_login/src/test/java/com/dalaoyang/Springboot2RedisLoginApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dalaoyang;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2RedisLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+
+}
